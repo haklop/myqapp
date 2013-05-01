d79da437f81c6f3e3427913ccaaeb9e52659fea1
Julien Vey <vey.julien@gmail.com>
2013-05-01 16:13:55 +0200

    Branchement du FeedService avec le FeedController

--- src/main/java/com/infoq/myqapp/controller/FeedController.java
+++ src/main/java/com/infoq/myqapp/controller/FeedController.java
@@ -1,11 +1,13 @@
 package com.infoq.myqapp.controller;
 
 import com.infoq.myqapp.domain.FeedEntry;
+import com.infoq.myqapp.service.FeedService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +15,12 @@ import java.util.List;
 @RequestMapping("/feed")
 public class FeedController {
 
-    @RequestMapping(method =  RequestMethod.GET)
+    @Resource
+    private FeedService feedService;
+
+    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public List<FeedEntry> getAllBoxes(){
-        return new ArrayList<>();
+    public List<FeedEntry> getAllBoxes() throws Exception {
+        return feedService.readFeed();
     }
 }
