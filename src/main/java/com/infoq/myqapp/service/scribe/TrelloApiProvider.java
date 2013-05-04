44c6e685a31f367e320df5cd455068ed146ebc0c
Eric Bellemon <eric.bellemon@gmail.com>
2013-05-04 16:36:34 +0200

    Ajout du param name dans l'url Trello

--- src/main/java/com/infoq/myqapp/service/scribe/TrelloApiProvider.java
+++ src/main/java/com/infoq/myqapp/service/scribe/TrelloApiProvider.java
@@ -3,25 +3,21 @@ package com.infoq.myqapp.service.scribe;
 import org.scribe.builder.api.DefaultApi10a;
 import org.scribe.model.Token;
 
-public class TrelloApiProvider extends DefaultApi10a
-{
-    private static final String AUTHORIZE_URL = "https://trello.com/1/OAuthAuthorizeToken?scope=read,write&oauth_token=%s";
+public class TrelloApiProvider extends DefaultApi10a {
+    private static final String AUTHORIZE_URL = "https://trello.com/1/OAuthAuthorizeToken?scope=read,write&oauth_token=%s&name=MyQApp";
 
     @Override
-    public String getAccessTokenEndpoint()
-    {
+    public String getAccessTokenEndpoint() {
         return "https://trello.com/1/OAuthGetAccessToken";
     }
 
     @Override
-    public String getRequestTokenEndpoint()
-    {
+    public String getRequestTokenEndpoint() {
         return "https://trello.com/1/OAuthGetRequestToken";
     }
 
     @Override
-    public String getAuthorizationUrl(Token requestToken)
-    {
+    public String getAuthorizationUrl(Token requestToken) {
         return String.format(AUTHORIZE_URL, requestToken.getToken());
     }
 
