4477b0679f06722ef9b354e8ff3b6680ff3a24e7
Julien Vey <vey.julien@gmail.com>
2013-05-16 15:27:02 +0200

    Affichage uniquement des conferences a venir

--- src/main/java/com/infoq/myqapp/service/ConferenceService.java
+++ src/main/java/com/infoq/myqapp/service/ConferenceService.java
@@ -3,16 +3,24 @@ package com.infoq.myqapp.service;
 import com.infoq.myqapp.domain.Conference;
 import com.infoq.myqapp.repository.ConferenceRepository;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Service
 public class ConferenceService {
 
     @Resource
     private ConferenceRepository conferenceRepository;
+
+    @Resource
+    private MongoTemplate mongoTemplate;
 
     public void createConf(Conference conference) {
         // TODO check conference
@@ -21,6 +29,10 @@ public class ConferenceService {
     }
 
     public List<Conference> getAllConfs() {
-        return conferenceRepository.findAll(new Sort(Sort.Direction.ASC, "startDate"));
+        return mongoTemplate.find(query(where("endDate").gte(yesterday())).with(new Sort(Sort.Direction.ASC, "startDate")), Conference.class);
+    }
+
+    private Date yesterday() {
+        return new Date(new Date().getTime() - (1000 * 60 * 60 * 24));
     }
 }
