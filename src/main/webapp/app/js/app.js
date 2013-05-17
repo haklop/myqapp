d3e8a81ff3b27918d9a064cbae755d6bce9e3338
Julien Vey <vey.julien@gmail.com>
2013-05-17 15:58:27 +0200

    refactoring pour utiliser les directives angular natives

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -28,16 +28,3 @@ module.factory('http401Interceptor', function ($q) {
 module.config(function ($httpProvider) {
     $httpProvider.responseInterceptors.push('http401Interceptor');
 });
-
-module.directive('ngIf', function() {
-    return {
-        link: function(scope, element, attrs) {
-            if(scope.$eval(attrs.ngIf)) {
-                // remove '<div ng-if...></div>'
-                element.replaceWith(element.children())
-            } else {
-                element.replaceWith(' ')
-            }
-        }
-    }
-});
