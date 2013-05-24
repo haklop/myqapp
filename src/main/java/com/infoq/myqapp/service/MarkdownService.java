0694b674a0498fcc8d1ec87c0402e267633339ee
Eric Bellemon <eric.bellemon@gmail.com>
2013-05-25 00:07:32 +0200

    Nettoyga du spam octicon

--- src/main/java/com/infoq/myqapp/service/MarkdownService.java
+++ src/main/java/com/infoq/myqapp/service/MarkdownService.java
@@ -1,6 +1,9 @@
 package com.infoq.myqapp.service;
 
 import com.infoq.myqapp.domain.GitHubMarkdown;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +13,17 @@ public class MarkdownService {
     private RestTemplate restTemplate = new RestTemplate();
 
     public String generateHtml(GitHubMarkdown markdown) {
-        return restTemplate.postForObject("https://api.github.com/markdown", markdown, String.class);
+        String result =  restTemplate.postForObject("https://api.github.com/markdown", markdown, String.class);
+
+        return processHtml(result);
+    }
+
+    private String processHtml(String html) {
+        Document myAmazingContent = Jsoup.parse(html);
+
+        for (Element octiconSpan : myAmazingContent.select("span.octicon")) {
+            octiconSpan.remove();
+        }
+        return myAmazingContent.body().outerHtml().replace("<body>","").replace("</body>", "");
     }
 }
