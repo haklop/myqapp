4db08211e4b21725a6d5478f996fa2659e010c15
Julien Vey <vey.julien@gmail.com>
2013-05-27 17:56:43 +0200

    Ajout de l'url github (recuperation de l'url raw + authent github a faire)

--- src/main/webapp/app/js/controller/mardownGeneratorController.js
+++ src/main/webapp/app/js/controller/mardownGeneratorController.js
@@ -15,12 +15,36 @@ function MarkdownGeneratorCtrl($scope, MarkdownGenerator, TrelloList) {
         for(var i =0; i< result.cards.length; i++){
             var card = result.cards[i];
             if(card.desc.indexOf("github.com") !== -1){
+                var urls = findUrls(card.desc);
+                for(var j =0; j< urls.length; j++){
+                    if(urls[j].indexOf("github.com") !== -1){
+                        card.githubUrl = urls[j];
+                    }
+                }
                 $scope.cards.push(card);
             } else {
                 $scope.cardsNoGithub.push(card);
             }
         }
-
-
     })
+
+    function findUrls( text )
+    {
+        var source = (text || '').toString();
+        var urlArray = [];
+        var url;
+        var matchArray;
+
+        // Regular expression to find FTP, HTTP(S) and email URLs.
+        var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g;
+
+        // Iterate through any URLs in the text.
+        while( (matchArray = regexToken.exec( source )) !== null )
+        {
+            var token = matchArray[0];
+            urlArray.push( token );
+        }
+
+        return urlArray;
+    }
 }
