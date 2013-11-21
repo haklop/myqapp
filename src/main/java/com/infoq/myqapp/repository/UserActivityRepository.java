8072bf744e8621ad6d316a5dde515803b549152a
Eric Bellemon <eric.bellemon@gmail.com>
2013-11-21 16:57:33 +0100

    Remove deprecated models

--- src/main/java/com/infoq/myqapp/repository/UserActivityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.infoq.myqapp.repository;
-
-import com.infoq.myqapp.domain.TrelloActivity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-@Deprecated
-public interface UserActivityRepository extends MongoRepository<TrelloActivity, String> {
-}
